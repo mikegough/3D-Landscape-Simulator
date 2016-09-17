21a42eb1aaf8c7d4637193240390c3913d7c6d4b
Taylor Mutch <taylormutch@gmail.com>
2016-09-17 15:27:48 -0700

    Revamped vegtype, elevation encoding. Now using bitshifts across multiple bands.

--- OutputProcessing/texture_utils.py
+++ OutputProcessing/texture_utils.py
@@ -5,8 +5,50 @@ import numpy_indexed as npi
 from PIL import Image
 
 
+def elevation_texture(elev_path):
+    """ Creates an elevation-encoded image from a given elevation GeoTiff
+    :param elev_path: The path to the elevation to encode into the texture.
+    """
+    with rasterio.open(elev_path, 'r') as src:
+
+        elev_data = src.read(1).astype('int16')
+        shape = elev_data.shape
+        elev_flat = elev_data.ravel()
+
+        # Pack the elevation into the color channels.
+        # Allows for a max of a 24 unsigned integer, and 1 8bit integer for depth below 0
+        unique_codes = [x for x in range(np.min(elev_data), np.max(elev_data)+1)]
+        r_map = dict()
+        g_map = dict()
+        b_map = dict()
+        a_map = dict()
+        for value in list(unique_codes):
+            if value >= 0:
+                r_map[value] = (value & 0xFF)
+                g_map[value] = (value & 0xFF00) >> 8
+                b_map[value] = (value & 0xFF0000) >> 16
+                a_map[value] = 255
+            elif value < 255:
+                r_map[value] = g_map[value] = b_map[value] = a_map[value] = 0
+            else:
+                r_map[value] = g_map[value] = b_map[value] = 0
+                a_map[value] = 255 + value  # value is negative at this point, so add it
+
+        image_shape = (shape[1], shape[0])  # images are column major
+        texture = Image.new('RGBA', image_shape)
+        r_array = npi.remap(elev_flat, list(r_map.keys()), list(r_map.values()))
+        g_array = npi.remap(elev_flat, list(g_map.keys()), list(g_map.values()))
+        b_array = npi.remap(elev_flat, list(b_map.keys()), list(b_map.values()))
+        a_array = npi.remap(elev_flat, list(a_map.keys()), list(a_map.values()))
+        image_data = [(r_array[i], g_array[i], b_array[i], a_array[i]) for i in range(shape[0] * shape[1])]
+        texture.putdata(image_data)
+        return texture
+
+
 def vegtype_texture(strata_path):
-    """ Creates a greyscale image from a given strata GeoTiff """
+    """ Creates a type-encoded image from a given strata GeoTiff
+    :param strata_path: The path to the vegtype to encode into the texture.
+    """
 
     with rasterio.open(strata_path, 'r') as src:
 
@@ -14,47 +56,31 @@ def vegtype_texture(strata_path):
         shape = strata_data.shape
         strata_flat = strata_data.ravel()
 
-        keys = np.unique(strata_flat)
-        mapping = dict()
-
-        # 0 should map to 0
-        # -9999 should map to 0
-        # otherwise, map key to value directly
-        for key in keys:
-            value = key
-            if key == -9999:
-                value = 0
-            mapping[key] = value  # * 30 # debug, to check that things are actually there
+        # Pack the codes into the color channels.
+        # Allows for a max of a 24 unsigned integer.
+        unique_codes = np.unique(strata_flat)
+        r_map = dict()
+        g_map = dict()
+        b_map = dict()
+        for value in list(unique_codes):
+            r_map[value] = (value & 0xFF)
+            g_map[value] = (value & 0xFF00) >> 8
+            b_map[value] = (value & 0xFF0000) >> 16
 
         image_shape = (shape[1], shape[0])  # images are column major
-        texture = Image.new('L', image_shape)
-
-        image_data = npi.remap(strata_flat, list(mapping.keys()), list(mapping.values()))
+        texture = Image.new('RGB', image_shape)
+        r_array = npi.remap(strata_flat, list(r_map.keys()), list(r_map.values()))
+        g_array = npi.remap(strata_flat, list(g_map.keys()), list(g_map.values()))
+        b_array = npi.remap(strata_flat, list(b_map.keys()), list(b_map.values()))
+        image_data = [(r_array[i], g_array[i], b_array[i]) for i in range(shape[0] * shape[1])]
         texture.putdata(image_data)
         return texture
 
 
-def stateclass_colors_as_list(colormap_path):
-
-    colors = list()
-
-    with open(colormap_path, 'r') as f:
-
-        colors_src = f.readlines()[2:]
-        for line in colors_src:
-            line = line.strip().split(',')
-            color = {
-                'value': "rgb(" + ",".join([line[1], line[2], line[3]]) + ")",
-                'name': line[5]
-            }
-
-            colors.append(color)
-
-    return colors
-
-
-def create_stateclass_texture(sc_tif, colormap):
-
+def stateclass_texture(sc_tif, colormap):
+    """ Creates a true-color image from a given strata GeoTiff
+    :param strata_path: The path to the vegtype to encode into the texture.
+    """
     r_map = dict()
     g_map = dict()
     b_map = dict()
@@ -99,14 +125,14 @@ def process_stateclass_directory(dir_path, sc_defs):
     :return:
     """
 
-    colormap = list()
+    colormap = list()   # TODO - let colors be determined by the UI or by the user?
     for stateclass in sc_defs.keys():
         color = sc_defs[stateclass]['Color'].split(',')
         r = color[1]
         g = color[2]
         b = color[3]
         idx = sc_defs[stateclass]['ID']
-        colormap.append({'ID':idx, 'r': r, 'g': g, 'b': b})
+        colormap.append({'ID': idx, 'r': r, 'g': g, 'b': b})
 
     file_names = os.listdir(dir_path)
     for f_name in file_names:
@@ -117,6 +143,7 @@ def process_stateclass_directory(dir_path, sc_defs):
             # parse iteration, timestep
             iteration = int(name_parts[0][2:])
             timestep = int(name_parts[1][2:])
-            texture = create_stateclass_texture(os.path.join(dir_path, f_name), colormap)
-            output_path = os.path.join(dir_path, 'stateclass_{timestep}.png'.format(timestep=timestep))
+            texture = stateclass_texture(os.path.join(dir_path, f_name), colormap)
+            output_path = os.path.join(dir_path, 'stateclass_{iteration}_{timestep}.png'.format(iteration=iteration,
+                                                                                                timestep=timestep))
             texture.save(output_path)
